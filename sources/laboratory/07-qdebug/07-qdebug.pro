3b5d1aca13347e2844ae2cf6835ff72e046fba06
Martin Rotter <rotter.martinos@gmail.com>
2013-01-27 13:57:15 +0100

    NEW

---
+++ sources/laboratory/07-qdebug/07-qdebug.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-01-27T11:56:16
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 07-qdebug
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ sources/laboratory/07-qdebug/main.cpp
@@ -0,0 +1,59 @@
+#include <QCoreApplication>
+#include <QThread>
+#include <QDebug>
+
+#define APP_LOW_NAME "NAME"
+
+
+void debug_handler(QtMsgType type, const QMessageLogContext &placement, const QString &message) {
+    switch (type) {
+	case QtDebugMsg:
+	    fprintf(stderr, "[%s] INFO (%s, line %d) : %s\n",
+		    APP_LOW_NAME,
+		    placement.file,
+		    placement.line,
+		    qPrintable(message));
+	    break;
+	case QtWarningMsg:
+	    fprintf(stderr, "[%s] WARNING (%s, line %d) : %s\n",
+		    APP_LOW_NAME,
+		    placement.file,
+		    placement.line,
+		    qPrintable(message));
+	    break;
+	case QtCriticalMsg:
+	    fprintf(stderr, "[%s] CRITICAL (%s, line %d) : %s\n",
+		    APP_LOW_NAME,
+		    placement.file,
+		    placement.line,
+		    qPrintable(message));
+	    break;
+	case QtFatalMsg:
+	    fprintf(stderr, "[%s] FATAL (%s, line %d) : %s\nApplication is halting now.\n",
+		    APP_LOW_NAME,
+		    placement.file,
+		    placement.line,
+		    qPrintable(message));
+	    qApp->exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+    
+    qInstallMessageHandler(debug_handler);
+
+    qputenv("Q_MESSAGE_PATTERN",
+	    "[%{type}] %{appname} (%{file}:%{line}) - %{message}");
+
+    qDebug() << "First information.";
+    qDebug("Second information from %s.", "Martin");
+
+    // Printing out custom type.
+    qWarning() << QThread::currentThreadId();
+
+    // No way to continue.
+    qFatal("This is custom error message.");
+
+    return a.exec();
+}
