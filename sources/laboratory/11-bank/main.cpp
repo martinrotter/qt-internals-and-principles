63c9d9528ff24b57ed2a6fb54254853883a72ff5
Martin Rotter <rotter.martinos@gmail.com>
2013-02-07 12:45:35 +0100

    typo.

--- sources/laboratory/11-bank/main.cpp
+++ sources/laboratory/11-bank/main.cpp
@@ -8,5 +8,25 @@
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
+
+    // Create bank.
+    Bank bank(&a);
+
+    // Create two accounts with some initial cash and register both accounts in the bank.
+    Account acc_mar("Martin", 15000, &bank);
+    Account acc_car("Carl", 11000, &bank);
+
+    bank.printAccounts();
+
+    bank.transfer("Martin", "Carl", 12000);
+    bank.printAccounts();
+
+    bank.transfer("Carl", "Martin", 2000);
+    bank.printAccounts();
+
+    bank.transfer("Carl", "Martin", 25000);
+    bank.printAccounts();
+
+    qDebug("Application exit.");
     return a.exec();
 }
